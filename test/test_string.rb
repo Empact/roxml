594daf66d6b81eb3e89af906bc0a4b9dd03a4dca
Ben Woosley <ben.woosley@gmail.com>
2008-08-28 00:21:53 -0700

    test access to string extensions

---
+++ test/test_string.rb
@@ -0,0 +1,11 @@
+require 'lib/roxml'
+
+class TestROXML < Test::Unit::TestCase
+  def test_to_latin_is_accessible
+    assert String.instance_methods.include?('to_latin')
+  end
+  
+  def test_to_utf_is_accessible
+    assert String.instance_methods.include?('to_utf')
+  end
+end

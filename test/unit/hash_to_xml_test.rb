39e24736835cf5223a49c0ef65a725c96af8163f
Ben Woosley <ben.woosley@gmail.com>
2008-09-09 23:22:55 -0700

    We're going to have a bunch of to_xml tests, so we metaprogram it up

--- test/unit/hash_to_xml_test.rb
+++ test/unit/hash_to_xml_test.rb
@@ -1,9 +1,5 @@
 require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class TestHashToXml < Test::Unit::TestCase
-  def test_dictionary_of_attrs
-    name = :dictionary_of_attrs
-    dict = DictionaryOfAttrs.parse(fixture(name))
-    assert_equal xml_fixture(name), dict.to_xml
-  end
+  to_xml_test :dictionary_of_attrs
 end

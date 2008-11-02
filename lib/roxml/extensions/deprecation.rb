f6e18fc5c30faf05c45d7214830794203b600a33
Ben Woosley <ben.woosley@gmail.com>
2008-11-02 16:37:42 +0100

    Only fix ActiveSupport deprecation message pre-2.2, and fix it to the actual message which will be used in 2.2.

--- lib/roxml/extensions/deprecation.rb
+++ lib/roxml/extensions/deprecation.rb
@@ -1,8 +1,14 @@
+require 'active_support'
+require 'active_support/version'
+
 module ActiveSupport # :nodoc:all
-  module Deprecation
-    class << self
-      def deprecation_message(callstack, message = nil)
-        "DEPRECATION WARNING: #{message} #{deprecation_caller_message(callstack)}"
+  if VERSION::MAJOR <= 2 && VERSION::MINOR <= 1
+    module Deprecation
+      class << self
+        def deprecation_message(callstack, message = nil)
+          message ||= "You are using deprecated behavior which will be removed from the next major or minor release."
+          "DEPRECATION WARNING: #{message}. #{deprecation_caller_message(callstack)}"
+        end
       end
     end
   end

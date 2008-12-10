4a4bd88514e298e1b7651d340e74a2de808ca311
Ben Woosley <ben.woosley@gmail.com>
2008-12-09 22:58:23 -0800

    Move to_xml_test into test_helper for use elsewhere

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,28 @@ end
 def fixture_path(name)
   "test/fixtures/#{name}.xml"
 end
+
+def to_xml_test(*names)
+  names = names.only if names.one? && names.only.is_a?(Hash)
+  names.each do |(name, xml_name)|
+    xml_name ||= name
+
+    define_method "test_#{name}" do
+      dict = name.to_s.camelize.constantize.from_xml(fixture(xml_name))
+      xml = xml_fixture(xml_name)
+      remove_children(xml)
+      assert_equal xml, dict.to_xml
+    end
+  end
+end
+
+def remove_children(xml)
+  return unless xml.respond_to? :children
+  xml.children.each do |child|
+    if child.blank?
+      child.remove!
+    else
+      remove_children(child)
+    end
+  end
+end
--- test/unit/to_xml_test.rb
+++ test/unit/to_xml_test.rb
@@ -1,30 +1,5 @@
 require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
-def to_xml_test(*names)
-  names = names.only if names.one? && names.only.is_a?(Hash)
-  names.each do |(name, xml_name)|
-    xml_name ||= name
-
-    define_method "test_#{name}" do
-      dict = name.to_s.camelize.constantize.from_xml(fixture(xml_name))
-      xml = xml_fixture(xml_name)
-      remove_children(xml)
-      assert_equal xml, dict.to_xml
-    end
-  end
-end
-
-def remove_children(xml)
-  return unless xml.respond_to? :children
-  xml.children.each do |child|
-    if child.blank?
-      child.remove!
-    else
-      remove_children(child)
-    end
-  end
-end
-
 class TestHashToXml < Test::Unit::TestCase
   to_xml_test :dictionary_of_attrs,
               :dictionary_of_mixeds,

1c32e952e4aa99412d586ecd6a5cf1466e2bf4bd
Ben Woosley <ben.woosley@gmail.com>
2009-01-31 12:17:18 -0500

    Example current_weather_spec passes

--- examples/current_weather.rb
+++ examples/current_weather.rb
@@ -1,22 +1,26 @@
-dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
-require File.join(dir, 'happymapper')
+require File.join(File.dirname(__FILE__), '../lib/roxml')
 
-file_contents = File.read(dir + '/../spec/fixtures/current_weather.xml')
-
-class CurrentWeather
+class WeatherObservation
   include ROXML
   xml_convention :dasherize
   
   xml_name 'ob'
-  namespace 'aws'
+  xml_namespace 'aws'
   xml_reader :temperature, :as => Integer, :from => 'temp'
   xml_reader :feels_like, :as => Integer
   xml_reader :current_condition, :attributes => {:icon => String}
 end
 
-CurrentWeather.parse(file_contents).each do |current_weather|
-  puts "temperature: #{current_weather.temperature}"
-  puts "feels_like: #{current_weather.feels_like}"
-  puts "current_condition: #{current_weather.current_condition}"
-  puts "current_condition.icon: #{current_weather.current_condition.icon}"
+class WeatherObservations
+  include ROXML
+  xml_reader :observations, [WeatherObservation], :from => 'ob'
+end
+
+unless defined?(Spec)
+  WeatherObeservations.from_xml(file_contents).each do |current_weather|
+    puts "temperature: #{current_weather.temperature}"
+    puts "feels_like: #{current_weather.feels_like}"
+    puts "current_condition: #{current_weather.current_condition}"
+    puts "current_condition.icon: #{current_weather.current_condition.icon}"
+  end
 end
--- spec/examples/current_weather_spec.rb
+++ spec/examples/current_weather_spec.rb
@@ -1,4 +1,26 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+require File.join(File.dirname(__FILE__), '../spec_helper')
+require example('current_weather')
 
-puts "Hello World"
+describe WeatherObservation do
+  before do
+    @observations = WeatherObservations.from_xml(xml_for('current_weather')).observations
+  end
+
+  it "should extract temperature" do
+    @observations.each {|observation| observation.temperature.should > 0 }
+  end
+
+  it "should extract feels_like" do
+    @observations.each {|observation| observation.feels_like.should > 0 }
+  end
+
+  describe "#current_condition" do
+    it "should extract current_condition" do
+      @observations.each {|observation| observation.current_condition.should_not be_empty }
+    end
+
+    it "should extract icon attribute" do
+      @observations.each {|observation| observation.current_condition.should_not be_empty }
+    end
+  end
+end

b340e9a53a5c31416740d51f48f391d8c3bd11c1
Ben Woosley <ben.woosley@gmail.com>
2009-03-09 02:08:24 -0400

    Require active_support directly, as its less brittle and gets the internal active_support dependencies right.

--- lib/roxml/extensions.rb
+++ lib/roxml/extensions.rb
@@ -1,3 +1,6 @@
-%w(active_support deprecation array string).each do |file|
+require 'rubygems'
+require 'active_support'
+
+%w(deprecation array string).each do |file|
   require File.join(File.dirname(__FILE__), 'extensions', file)
 end

a8b40cbae423f7c4d6f7460d4abf4bb1849c97dc
Ben Woosley <ben.woosley@gmail.com>
2009-10-10 20:50:07 -0400

    Remove ActiveSupport::Deprecation extensions, as we're done using deprecation

--- lib/roxml/extensions.rb
+++ lib/roxml/extensions.rb
@@ -1,6 +1,4 @@
 require 'rubygems'
 require 'active_support'
 
-%w(deprecation array).each do |file|
-  require File.join(File.dirname(__FILE__), 'extensions', file)
-end
+require File.join(File.dirname(__FILE__), 'extensions', 'array')

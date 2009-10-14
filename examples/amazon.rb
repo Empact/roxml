b1e3b7833a0038148bfae11897ed4c870138f2cd
Ben Woosley <ben.woosley@gmail.com>
2009-10-13 20:30:41 -0400

    With some namespace adjustment, Amazon spec passes in Nokogiri.  Need some work though to remain user-friendly.

--- examples/amazon.rb
+++ examples/amazon.rb
@@ -7,27 +7,30 @@ module PITA
   class Base
     include ROXML
     xml_convention :camelcase
+    xml_namespace :xmlns
   end
 
   class Item < Base
-    xml_reader :asin, :from => 'ASIN'
-    xml_reader :detail_page_url, :from => 'DetailPageURL'
-    xml_reader :manufacturer, :in => './'
+    xml_reader :asin, :from => 'xmlns:ASIN'
+    xml_reader :detail_page_url, :from => 'xmlns:DetailPageURL'
+    xml_reader :manufacturer, :in => 'xmlns:ItemAttributes'
     # this is the only xml_reader that exists in a different namespace, so it
     # must be explicitly specified
     xml_reader :point, :from => 'georss:point'
   end
 
   class ItemSearchResponse < Base
-    xml_reader :total_results, :as => Integer, :in => 'Items'
-    xml_reader :total_pages, :as => Integer, :in => 'Items'
+    xml_reader :total_results, :as => Integer, :in => 'xmlns:Items'
+    xml_reader :total_pages, :as => Integer, :in => 'xmlns:Items'
     xml_reader :items, :as => [Item]
   end
 end
 
 unless defined?(Spec)
-  item = PITA::ItemSearchResponse.from_xml(xml_for('amazon'))
-  item.items.each do |i|
-    puts i.asin, i.detail_page_url, i.manufacturer, ''
+  response = PITA::ItemSearchResponse.from_xml(xml_for('amazon'))
+  p response.total_results
+  p response.total_pages 
+  response.items.each do |i|
+    puts i.asin, i.detail_page_url, i.manufacturer, i.point, ''
   end
 end

ab901f4aa635f02d162b065af2874504b7198cfb
Ben Woosley <ben.woosley@gmail.com>
2011-11-25 02:17:37 -0600

    We need the full inflector including its default inflections in able to properly inflect by default

--- lib/roxml.rb
+++ lib/roxml.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 require 'active_support'
 if Gem.loaded_specs['activesupport'].version >= Gem::Version.new('3')
-  require 'active_support/inflector/inflections'
+  require 'active_support/inflector'
   require 'active_support/core_ext/object/duplicable'
   require 'active_support/core_ext/module/delegation'
   require 'active_support/core_ext/array/extract_options'

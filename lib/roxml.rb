329d782f850474da3a27e4301e6fe52e632613fd
Tim Riley <tim@openmonkey.com>
2019-07-04 16:28:02 +1000

    Remove ActiveSupport starts_ends_with core ext

--- lib/roxml.rb
+++ lib/roxml.rb
@@ -4,7 +4,6 @@ require 'active_support'
 if Gem.loaded_specs['activesupport'] && Gem.loaded_specs['activesupport'].version >= Gem::Version.new('3')
   require 'active_support/inflector'
   require 'active_support/core_ext/hash'
-  require 'active_support/core_ext/string/starts_ends_with'
 end
 
 require 'roxml/definition'

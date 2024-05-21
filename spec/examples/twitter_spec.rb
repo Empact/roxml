40428a740dc2fa124c077ba4c6c2850d758c3a20
Benjamin Woosley <ben.woosley@gmail.com>
2024-05-21 13:11:17 -0500

    test: Rework nesting of describes in twitter_spec to avoid exception
    undefined method `=~' for class User/Status

--- spec/examples/twitter_spec.rb
+++ spec/examples/twitter_spec.rb
@@ -2,31 +2,29 @@ require 'spec_helper'
 require_relative './../../examples/twitter'
 
 describe Statuses do
-  describe Status do
+  before do
+    @statuses = Statuses.from_xml(xml_for('twitter')).statuses
+  end
+
+  it "should extract text" do
+    @statuses.each {|status| expect(status.text).to_not be_empty }
+  end
+
+  it "should extract source" do
+    @statuses.each {|status| expect(status.source).to_not be_empty }
+  end
+
+  describe "#user" do
     before do
-      @statuses = Statuses.from_xml(xml_for('twitter')).statuses
+      @users = @statuses.map(&:user)
     end
 
-    it "should extract text" do
-      @statuses.each {|status| expect(status.text).to_not be_empty }
+    it "should extract name" do
+      @users.each {|user| expect(user.name).to eq("John Nunemaker") }
     end
 
-    it "should extract source" do
-      @statuses.each {|status| expect(status.source).to_not be_empty }
-    end
-
-    describe User do
-      before do
-        @users = @statuses.map(&:user)
-      end
-
-      it "should extract name" do
-        @users.each {|user| expect(user.name).to eq("John Nunemaker") }
-      end
-
-      it "should extract screen_name" do
-        @users.each {|user| expect(user.screen_name).to eq("jnunemaker") }
-      end
+    it "should extract screen_name" do
+      @users.each {|user| expect(user.screen_name).to eq("jnunemaker") }
     end
   end
 end
